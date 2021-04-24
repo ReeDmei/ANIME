5f96977373a950d2b097d95060cc4d986d3b9726
ReeDmei <83131063+ReeDmei@users.noreply.github.com>
2021-04-24 12:45:28 -0500

    ANIMEv4
    this is anime v4

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const http = require ('http');
+const bodyParser = require ('body-parser');
+const mysql = require ('mysql');
+const app = express();
+app.use (bodyParser.json());
+const pool = mysql.createPool ({
+    connectLimit : 10,
+    host            : '98.10.161.9',
+    user            : 'demi',
+    password        : 'poggers123!',
+    database        : 'poggers'   
+})
+app.get('/', (req, res) => {
+    res.send (JSON.stringify({"ALEX": "AVIER"}))
+})
+
+
+const httpServer = http.createServer (app);
+httpServer.listen(8080, () =>{
+    console.log ("a");
+})
